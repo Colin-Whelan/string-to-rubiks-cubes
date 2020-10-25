915870056903c25d0f9b6361c11ce4a74b8021a8
Colin-Whelan <38737123+Colin-Whelan@users.noreply.github.com>
2020-10-25 19:21:58 -0400

    map option

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const braille = require('./node_modules/bin-braille-parser/index.js')
 
-secretMessage = process.argv[2]
-if (secretMessage != true) {
+var codeString = ''
+var secretMessage = process.argv[2]
+if (!secretMessage) {
   secretMessage = 'Hello world.'
 }
-var secretMessage = braille.parserTextToBinBraille(secretMessage)
 
+var map = process.argv[3]
+if (!map) {
+  map = 'color'
+}
+secretMessage = braille.parserTextToBinBraille(secretMessage)
 
 var binaryString = ''
 
@@ -17,42 +22,78 @@ for (var letter = 0; letter < secretMessage.length; letter++) {
   };
 };
 
-var colorMap = []
+var colormap = []
+var color
+
+var keymap = []
+var key
 
 for (var i = 0; i < binaryString.length; i++) {
   switch (i % 6) {
     case 0:
       color = 'blue'
+      key = '^'
       break
     case 1:
       color = 'green'
+      key = 'v'
       break
     case 2:
       color = 'orange'
+      key = '<'
       break
     case 3:
       color = 'red'
+      key = '>'
       break
     case 4:
       color = 'yellow'
+      key = '•'
       break
     case 5:
       color = 'black'
+      key = 'X'
       break
   }
 
-  colorMap.push(color + binaryString[i])
-
+  colormap.push(color + binaryString[i])
+  keymap.push(key + binaryString[i])
 };
 
-var cleanedString = ''
+function cleanString(string) {
+  string = string.replace(/1/g, '');
+  string = string.slice(0, -1)
+  return string
+}
 
-for (var i = 0; i < colorMap.length; i++) {
-  if (colorMap[i].includes('1')) {
-    cleanedString += colorMap[i] + ','
+function addToCodeString(mapChoice) {
+  for (var i = 0; i < mapChoice.length; i++) {
+    if (mapChoice[i].includes('1')) {
+      codeString += mapChoice[i] + ','
+    }
+  };
+}
+
+function createCodeString() {
+
+  switch (map) {
+    case 'color':
+      addToCodeString(colormap)
+      break
+    case 'key':
+      addToCodeString(keymap)
+      break
+    case 'all':
+      addToCodeString(colormap)
+      codeString += '\n\n'
+      addToCodeString(keymap)
+      break
+    case 'default':
+      addToCodeString(colormap)
+      break
   }
-};
+}
 
-cleanedString = cleanedString.replace(/1/g, '');
+createCodeString()
 
-console.log(cleanedString)
+console.log(cleanString(codeString))
