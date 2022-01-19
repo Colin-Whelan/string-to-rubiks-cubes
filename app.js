a57e1caaaf622664df8be94a192addc55f60fa95
Colin <clnwhelan+github@gmail.com>
2022-01-18 23:09:01 -0500

    reworked mapping logic

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const braille = require('./node_modules/bin-braille-parser/index.js')
 
-var codeString = ''
-
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
@@ -20,7 +18,7 @@ function logError(arg){
   process.exit(1)
 }
 
-var map = argv.map
+let map = argv.map
 if (!map) {
   console.log(`No --map= value detected, defaulting to 'color' map`)
   map = 'color'
@@ -29,111 +27,37 @@ map = map.toLowerCase()
 
 secretMessage = braille.parserTextToBinBraille(secretMessage)
 
-var binaryString = ''
+// false = braille dot turned off
+// true = braille dot turned on
+let binaryString = ''
 
-for (var letter = 0; letter < secretMessage.length; letter++) {
-  for (var characterRow = 0; characterRow < secretMessage[letter].length; characterRow++) {
-    for (var binary = 0; binary < secretMessage[letter][characterRow].length; binary++) {
+for (let letter = 0; letter < secretMessage.length; letter++) {
+  for (let characterRow = 0; characterRow < secretMessage[letter].length; characterRow++) {
+    for (let binary = 0; binary < secretMessage[letter][characterRow].length; binary++) {
       binaryString += secretMessage[letter][characterRow][binary]
     };
   };
 };
 
-// this can be improved
-var colormap = []
-var color
+let keys = {}
 
-var keymap = []
-var key
+keys.color = ['blue', 'green', 'orange', 'red', 'yellow', 'black']
+keys.symbol = ['^', 'v', '<', '>', '•', 'X']
+keys.number = [0,1,2,3,4,5]
 
-var numbermap = []
-var number
+let encodedSecret = []
 
 
-for (var i = 0; i < binaryString.length; i++) {
-  switch (i % 6) {
-    case 0:
-      color = 'blue'
-      key = '^'
-      number = '1'
-      break
-    case 1:
-      color = 'green'
-      key = 'v'
-      number = '2'
-      break
-    case 2:
-      color = 'orange'
-      key = '<'
-      number = '3'
-      break
-    case 3:
-      color = 'red'
-      key = '>'
-      number = '4'
-      break
-    case 4:
-      color = 'yellow'
-      key = '•'
-      number = '5'
-      break
-    case 5:
-      color = 'black'
-      key = 'X'
-      number = '6'
-      break
+for (let i = 0; i < binaryString.length; i++) {  
+  if (binaryString[i] == 1) {
+    // if that point in the braille matrix is enabled, use the keymap to assign char and push to array
+
+    let encodedChar = keys[map][i % 6]   
+    encodedSecret.push(encodedChar)
+
   }
-
-  colormap.push(color + binaryString[i])
-  keymap.push(key + binaryString[i])
-  numbermap.push(number + binaryString[i])
-
-  console.log(keymap)
 };
 
-function convertStringByMap(string, map) {
-  
-}
+encodedSecret = encodedSecret.toString()
 
-function cleanString(string) {
-  string = string.replace(/1/g, '');
-  string = string.slice(0, -1)
-  return string
-}
-
-function addToCodeString(mapChoice) {
-  for (var i = 0; i < mapChoice.length; i++) {
-    if (mapChoice[i].includes('1')) {
-      codeString += mapChoice[i] + ','
-    }
-  };
-}
-
-function createCodeString() {
-
-  switch (map) {
-    case 'color':
-      addToCodeString(colormap)
-      break
-    case 'key':
-      addToCodeString(keymap)
-      break
-    case 'number':
-      addToCodeString(numbermap)
-      break
-    case 'all':
-      addToCodeString(colormap)
-      codeString += '\n\n'
-      addToCodeString(keymap)
-      codeString += '\n\n'
-      addToCodeString(numbermap)
-      break
-    case 'default':
-      addToCodeString(colormap)
-      break
-  }
-}
-
-createCodeString()
-
-console.log(cleanString(codeString))
+console.log(encodedSecret)
