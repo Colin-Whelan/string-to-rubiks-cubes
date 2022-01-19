0ebbeadade9f78b47886c963dce38889bd2c02b0
Colin <clnwhelan+github@gmail.com>
2022-01-18 22:12:36 -0500

    working

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const braille = require('./node_modules/bin-braille-parser/index.js')
 
 var codeString = ''
-var secretMessage = process.argv[2]
-if (!secretMessage) {
-  secretMessage = 'Hello world.'
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
+doesArgExist(argv.t, 't')
+let secretMessage = argv.t
+
+function doesArgExist(arg, argName) {
+  if (!arg || arg === true) { 
+    logError(`--${argName}=`)
+  } 
 }
 
-var map = process.argv[3]
+function logError(arg){
+  console.error(`Enter a valid '${arg}'`)
+  process.exit(1)
+}
+
+var map = argv.map
 if (!map) {
+  console.log(`No --map= value detected, defaulting to 'color' map`)
   map = 'color'
 }
+map = map.toLowerCase()
+
 secretMessage = braille.parserTextToBinBraille(secretMessage)
 
 var binaryString = ''
@@ -22,43 +39,61 @@ for (var letter = 0; letter < secretMessage.length; letter++) {
   };
 };
 
+// this can be improved
 var colormap = []
 var color
 
 var keymap = []
 var key
 
+var numbermap = []
+var number
+
+
 for (var i = 0; i < binaryString.length; i++) {
   switch (i % 6) {
     case 0:
       color = 'blue'
       key = '^'
+      number = '1'
       break
     case 1:
       color = 'green'
       key = 'v'
+      number = '2'
       break
     case 2:
       color = 'orange'
       key = '<'
+      number = '3'
       break
     case 3:
       color = 'red'
       key = '>'
+      number = '4'
       break
     case 4:
       color = 'yellow'
       key = '•'
+      number = '5'
       break
     case 5:
       color = 'black'
       key = 'X'
+      number = '6'
       break
   }
 
   colormap.push(color + binaryString[i])
   keymap.push(key + binaryString[i])
+  numbermap.push(number + binaryString[i])
+
+  console.log(keymap)
 };
+
+function convertStringByMap(string, map) {
+  
+}
 
 function cleanString(string) {
   string = string.replace(/1/g, '');
@@ -83,10 +118,15 @@ function createCodeString() {
     case 'key':
       addToCodeString(keymap)
       break
+    case 'number':
+      addToCodeString(numbermap)
+      break
     case 'all':
       addToCodeString(colormap)
       codeString += '\n\n'
       addToCodeString(keymap)
+      codeString += '\n\n'
+      addToCodeString(numbermap)
       break
     case 'default':
       addToCodeString(colormap)
